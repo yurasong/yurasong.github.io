23dcf10a86b56a9f7c7d13a0d1be91d50de03643
yurasong <44369048+yurasong@users.noreply.github.com>
2026-05-09 10:26:54 +0000

    Deploying to gh-pages from @ yurasong/yurasong.github.io@0bd64d452b447915814c17ce75dc043d47b81750 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "If a full CV is required, please download it using the button at the top right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
