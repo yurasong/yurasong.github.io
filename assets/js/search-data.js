978086eb0157d915078f3c669ad3d5615b329de8
yurasong <44369048+yurasong@users.noreply.github.com>
2026-05-24 12:11:04 +0000

    Deploying to gh-pages from @ yurasong/yurasong.github.io@614dfcc47d6913a6833d66fa4ddd253f7a1239cc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Research projects, scientific collaborations, and community initiatives.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
