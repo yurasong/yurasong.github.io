47a2806482f83921dd7aafc8fb17fb6a16d7c963
yurasong <44369048+yurasong@users.noreply.github.com>
2026-05-24 11:50:25 +0000

    Deploying to gh-pages from @ yurasong/yurasong.github.io@93b98b729eb5180a88e8fb2e25dfc3593f716fdb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
+          description: "Workshops, invited lectures and training activities in bioinformatics and computational biology.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
